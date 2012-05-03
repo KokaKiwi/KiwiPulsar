a5f0f325185cc3b140beb6fc2b2d4e8a5086e5ef
KokaKiwi <admin@kokabsolu.com>
2012-05-03 16:54:15 +0200

    Data viewer

--- src/main/java/com/kokakiwi/fun/pulsar/web/PulsarServlet.java
+++ src/main/java/com/kokakiwi/fun/pulsar/web/PulsarServlet.java
@@ -1,16 +1,77 @@
 package com.kokakiwi.fun.pulsar.web;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
-public class PulsarServlet extends HttpServlet
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.kokakiwi.fun.pulsar.net.IDataListener;
+
+public class PulsarServlet extends HttpServlet implements IDataListener
 {
-    private static final long serialVersionUID = 6305415134358691775L;
+    private static final long  serialVersionUID = 6305415134358691775L;
+    
+    private final PulsarWeb    main;
+    
+    private final List<String> log              = Lists.newLinkedList();
+    
+    public PulsarServlet(PulsarWeb main)
+    {
+        this.main = main;
+        
+        main.getReceiver().setListener(this);
+    }
     
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException
     {
-        resp.getWriter().print("Hello from Java!\n");
+        String request = req.getPathInfo().substring(1);
+        if (!request.isEmpty())
+        {
+            String[] parts = request.split("/");
+            
+            if (parts[0].equals("log"))
+            {
+                for (String str : log)
+                {
+                    resp.getWriter().println(str);
+                }
+            }
+            else
+            {
+                resp.setStatus(404);
+                resp.getWriter().print("Not found!");
+            }
+        }
+    }
+    
+    public void onInfos(String name, double value)
+    {
+        
+    }
+    
+    public void onPower(double power)
+    {
+        
+    }
+    
+    public void onData(String[] datas)
+    {
+        String data = Joiner.on(' ').join(datas);
+        
+        log.add(data);
+    }
+    
+    public PulsarWeb getMain()
+    {
+        return main;
+    }
+    
+    public List<String> getLog()
+    {
+        return log;
     }
 }
--- src/main/java/com/kokakiwi/fun/pulsar/web/PulsarWeb.java
+++ src/main/java/com/kokakiwi/fun/pulsar/web/PulsarWeb.java
@@ -4,19 +4,58 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import com.kokakiwi.fun.pulsar.net.Receiver;
+
 public class PulsarWeb
 {
+    private final Server   server;
+    private final Receiver receiver;
     
-    public static void main(String[] args) throws Exception
+    public PulsarWeb()
     {
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(
                 ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
-        context.addServlet(new ServletHolder(new PulsarServlet()), "/*");
+        context.addServlet(new ServletHolder(new PulsarServlet(this)), "/*");
+        
+        receiver = new Receiver();
+    }
+    
+    public void start() throws Exception
+    {
+        receiver.start();
+        
         server.start();
         server.join();
+    }
+    
+    public void stop()
+    {
+        
+    }
+    
+    public Server getServer()
+    {
+        return server;
+    }
+    
+    public Receiver getReceiver()
+    {
+        return receiver;
+    }
+    
+    public static void main(String[] args)
+    {
+        try
+        {
+            new PulsarWeb().start();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
     
 }
